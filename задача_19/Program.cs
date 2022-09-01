9f75e79a1a0c4bcf3cc9add39b061b56945649f6
KEA <lena_konovalova@rambler.ru>
2022-09-01 16:25:28 +0300

    zadachi

---
+++ задача_19/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+Console.WriteLine("число");
+int num = Convert.ToInt32(Console.ReadLine());
+int temp = num;
+int rev = 0;
+int dig = 0;
+while(num > 0)
+{
+    dig = num % 10;
+    rev = rev * 10 + dig;
+    num = num /10;
+}
+    
+if(temp == rev)
+{
+    Console.WriteLine("Палидром");
+}
+
+
+Console.WriteLine("Не палидром");
+
---
+++ задача_23/Program.cs
@@ -0,0 +1,15 @@
+﻿string NumCub()
+{
+Console.WriteLine("Введите целое положительное число больше 0 ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+string result = "";
+
+while (i <= N)
+{
+    result = result + i*i*i + ", ";
+    i++;
+}
+return result;
+}
+Console.WriteLine(NumCub());
